b45da8504ad2c9e84b57f5219003dc94e14a51c7
Simon Green <simon.green@thielsen.co.uk>
2019-02-18 21:12:38 +0000

    Added second notelist test

--- test_all.js
+++ test_all.js
@@ -1,23 +1,29 @@
 // Test framework
 
-const describe = (description, test) => {
+function describe (description, test) {
   console.log(description)
   test()
 }
 
-const it = (message, func) => describe('  ' + message, func)
+function it (message, func) {
+  describe('  ' + message, func)
+}
 
-const matcher = (e) => ({
-  toBe: (assertion) => {
-    if (e === assertion) {
-      console.log('.')
-    } else {
-      console.log('F')
+function matcher (e) {
+  return {
+    toBe: function (assertion) {
+      if (e === assertion) {
+        console.log('.')
+      } else {
+        console.log('F')
+      }
     }
   }
-})
+}
 
-const expect = (e) => matcher(e)
+function expect (e) {
+  return matcher(e)
+}
 
 // Code
 
@@ -29,13 +35,35 @@ Note.prototype.read = function() {
   return this.text
 }
 
+function NoteList() {
+  this.list = []
+}
+
+NoteList.prototype.add = function(text) {
+  note = new Note(text)
+  this.list.push(note)
+}
+
+NoteList.prototype.returnNotes = function() {
+  return this.list
+}
+
 // Tests
 
 note = new Note('This is the first test note')
+notes = new NoteList()
 
 describe('note', () => {
   it('shows text when read', () => {
     const output = note.read()
     expect(output).toBe('This is the first test note')
+  })
+})
+
+describe('noteList', () => {
+  it('shows notes when added', () => {
+    notes.add('This is the first test note')
+    const output = notes.returnNotes()
+    expect(output[0]).toBe(note)
   })
 })
